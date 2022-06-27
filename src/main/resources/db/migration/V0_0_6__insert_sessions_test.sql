b032678b67589729c6b029f3c75a6fba068ad7b2
Ricardo Lopes <prslopes1@gmail.com>
2022-06-27 18:15:55 -0300

    removed static id insertion in migration

--- src/main/resources/db/migration/V0_0_6__insert_sessions_test.sql
+++ src/main/resources/db/migration/V0_0_6__insert_sessions_test.sql
@@ -1,6 +1,6 @@
-INSERT INTO tb_session (id, name, active, start_date, end_date, schedule_id) VALUES 
-(1, 'Test Session 1', true, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 1),
-(2, 'Test Session 2', true, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 2),
-(3, 'Test Session 3', true, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 3),
-(4, 'Test Session 4', true, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 4),
-(5, 'Test Session 5', false, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 5);
+INSERT INTO tb_session (name, active, start_date, end_date, schedule_id) VALUES 
+('Test Session 1', true, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 1),
+('Test Session 2', true, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 2),
+('Test Session 3', true, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 3),
+('Test Session 4', true, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 4),
+('Test Session 5', false, '2020-01-01 00:00:00', '2020-01-01 00:10:00', 5);
