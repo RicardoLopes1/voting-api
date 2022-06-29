9ac565d9607fce14a023104b184318a0a85b0fdc
Ricardo Lopes <prslopes1@gmail.com>
2022-06-29 10:54:05 -0300

    add documentation creation with swagger.

--- src/main/java/br/com/solutis/votingapi/VotingApiApplication.java
+++ src/main/java/br/com/solutis/votingapi/VotingApiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @EnableAsync
 @SpringBootApplication
 public class VotingApiApplication {
